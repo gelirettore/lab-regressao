18b1411959cef54d965011aef771754908ece7bb
gelirettore <gelirettore@gmail.com>
2019-04-30 15:50:58 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -13,61 +13,61 @@ import numpy as np
 data = "usina72.csv"
 
 def debug(text):
-    print(str(text));
+	print(str(text));
 
 def LinearRegr(X_train, X_test, y_train, y_test):
-    #Cria modelo
-    regr = LinearRegression()
-    
-    #Treina modelo
-    regr.fit(X_train, y_train)
-        
-    score_train = regr.score(X_train, y_train)
-    score_test = regr.score(X_test, y_test)
-    debug('Regressao Linear (treino): %.2f' % score_train)
-    debug('Regressao Linear (teste): %.2f' % score_test)
+	#Cria modelo
+	regr = LinearRegression()
+	
+	#Treina modelo
+	regr.fit(X_train, y_train)
+
+	score_train = regr.score(X_train, y_train)
+	score_test = regr.score(X_test, y_test)
+	debug('Regressao Linear (treino): %.2f' % score_train)
+	debug('Regressao Linear (teste): %.2f' % score_test)
 
 def SvrRegr(X_train, X_test, y_train, y_test):
-    #SVR + RBF
-    clf_rbf = SVR(kernel='rbf', gamma='scale', C=1.0, epsilon=0.1)
-    clf_rbf.fit(X_train, y_train)
-    score_rbf = clf_rbf.score(X_test, y_test)
-    predict_rbf = clf_rbf.predict(X_test)
-    print score_rbf
-    debug("SVR + RBF score: " + score_rbf)
+	#SVR + RBF
+	clf_rbf = SVR(kernel='rbf', gamma='scale', C=1.0, epsilon=0.1)
+	clf_rbf.fit(X_train, y_train)
+	score_rbf = clf_rbf.score(X_test, y_test)
+	predict_rbf = clf_rbf.predict(X_test)
+	print score_rbf
+	debug("SVR + RBF score: " + score_rbf)
 
 def main():
-    debug("Carregando dados")
-    #carrega dados e divide
-    
-    dados = pd.read_csv(data)
-    y1 = dados['f3']
-    y2 = dados['f5']
+	debug("Carregando dados")
+	#carrega dados e divide
 
-    x1 = dados['f4']
-    x2 = dados['f6']
-    x3 = dados['f9']
-    x4 = dados['f10']
-    x5 = dados['f11']
-    X = np.vstack((x1, x2,x3,x4,x5))
+	dados = pd.read_csv(data)
+	y1 = dados['f3']
+	y2 = dados['f5']
+	
+	x1 = dados['f4']
+	x2 = dados['f6']
+	x3 = dados['f9']
+	x4 = dados['f10']
+	x5 = dados['f11']
+	X = np.vstack((x1, x2,x3,x4,x5))
 
 	X_train, X_test, y1_train, y1_test, y2_train, y2_test = train_test_split(X, y1, y2, test_size=0.5, random_state=50)
-
-    #normalizando dados
-    min_max_scaler = preprocessing.MinMaxScaler()
+	
+	#normalizando dados
+	min_max_scaler = preprocessing.MinMaxScaler()
 	X_train_minmax = min_max_scaler.fit_transform(X_train)
 	X_test_minmax = min_max_scaler.fit_transform(X_test)
-    
-    y1_test_minmax = min_max_scaler.fit_transform(y1_test)
-    y1_train_minmax = min_max_scaler.fit_transform(y1_train)
+			
+	y1_test_minmax = min_max_scaler.fit_transform(y1_test)
+	y1_train_minmax = min_max_scaler.fit_transform(y1_train)
 
-    y2_test_minmax = min_max_scaler.fit_transform(y2_test)
-    y2_train_minmax = min_max_scaler.fit_transform(y2_train)
+	y2_test_minmax = min_max_scaler.fit_transform(y2_test)
+	y2_train_minmax = min_max_scaler.fit_transform(y2_train)
 
-    LinearRegr(X_train_minmax, X_test_minmax, y1_train, y1_test)
-    LinearRegr(X_train_minmax, X_test_minmax, y2_train, y2_test)
-#SvrRegr(X_train_minmax, X_test_minmax, y_train, y_test)
+	LinearRegr(X_train_minmax, X_test_minmax, y1_train, y1_test)
+	LinearRegr(X_train_minmax, X_test_minmax, y2_train, y2_test)
+	#SvrRegr(X_train_minmax, X_test_minmax, y_train, y_test)
 
 
 if __name__ == "__main__":
-    main()
+	main()
