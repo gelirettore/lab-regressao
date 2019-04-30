a93892cc8968ae9ea43af1fa6eb8d8b889f3078d
gelirettore <gelirettore@gmail.com>
2019-04-30 18:10:20 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPRegressor
 from sklearn import metrics
 import pandas as pd
 import numpy as np
@@ -12,6 +14,7 @@ import matplotlib.pyplot as plt
 
 
 data = "usina72.csv"
+kneighbors = 3
 
 def debug(text):
 	print(str(text));
@@ -43,6 +46,24 @@ def SvrRegr(X_train, X_test, y_train, y_test):
 	debug("SVR(rbf): " + str(metrics.mean_absolute_error(y_test, y_rbf)))
 	debug("SVR(linear): " + str(metrics.mean_absolute_error(y_test, y_lin)))
 	debug("SVR(poly): " + str(metrics.mean_absolute_error(y_test, y_poly)))
+#=======================
+def knnRegr(X_train, X_test, y_train, y_test):
+	debug("Calculando knn")
+	knn = KNeighborsClassifier(n_neighbors= kneighbors)
+	knn.fit(X_train,y_train)
+	y_pred = knn.predict(X_test)
+	debug("Knn: " + str(metrics.mean_absolute_error(y_test, y_pred)))
+
+#=======================
+def MlpRegr(X_train, X_test, y_train, y_test):
+	debug("Calculando MLP")
+	mlp = MLPRegressor(hidden_layer_sizes=(10,),  activation='relu', solver='adam',    alpha=0.001,batch_size='auto',
+					   learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=1000, shuffle=True,
+					   random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9,
+					   nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
+					   epsilon=1e-08)
+	mlp = mlp.fit(X_train, y_train)
+	mlp_pred = mlp.predict(X_test)
 
 #=======================
 def main():
@@ -71,8 +92,9 @@ def main():
 
 	LinearRegr(X_train_minmax, X_test_minmax, y1_train, y1_test)
 	LinearRegr(X_train_minmax, X_test_minmax, y2_train, y2_test)
-	SvrRegr(X_train_minmax, X_test_minmax, y1_train.reshape(-1,), y1_test.reshape(-1,))
-
+	#SvrRegr(X_train_minmax, X_test_minmax, y1_train.reshape(-1,), y1_test.reshape(-1,))
+	#SvrRegr(X_train_minmax, X_test_minmax, y2_train.reshape(-1,), y2_test.reshape(-1,))
+	knnRegr(X_train_minmax, X_test_minmax, y1_train, y1_test)
 
 if __name__ == "__main__":
 	main()
