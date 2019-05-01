9b46e89208ace423b2344e68fac1c20f691ff54e
gelirettore <gelirettore@gmail.com>
2019-05-01 11:13:19 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -63,11 +63,9 @@ def knnRegr(X_train, X_test, y_train, y_test):
 		for w in weight:
 			for m in metric:
 				if metric == 'mahalanobis':
-					params = "'V': np.cov(x)"
-			else:
-					params = "None"
-				
-				knn = KNeighborsRegressor(n_neighbors=n, weights=w, metric=m, n_jobs=15, metric_params=params)
+					knn = KNeighborsRegressor(n_neighbors=n, weights=w, metric=m, n_jobs=15, metric_params={'V': np.cov(X_train)})
+				else:
+					knn = KNeighborsRegressor(n_neighbors=n, weights=w, metric=m, n_jobs=15)
 				y_pred = knn.fit(X_train,y_train).predict(X_test)
 				mse =metrics.mean_squared_error(y_test, y_pred)
 				var = metrics.r2_score(y_test, y_pred)
