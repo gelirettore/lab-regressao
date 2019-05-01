9cb967a45e1b0a76e29300b1185a0bdbb3d0a1e0
gelirettore <gelirettore@gmail.com>
2019-05-01 09:58:14 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn import metrics, model_selection
+from sklearn import metrics, model_selection, linear_model
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,6 +21,13 @@ kneighbors = 3
 def debug(text):
 	print(str(text));
 
+def Validation(model, X_train, y_train):
+	loo = cross_validation.LeaveOneOut(len(y_train))
+	acc = cross_validation.cross_val_score(regr, X_train, y_train, scoring='accuracy', cv=loo)
+	fl = cross_validation.cross_val_score(regr, X_train, y_train, scoring='f1_weighted', cv=loo)
+	precision = cross_validation.cross_val_score(regr, X_train, y_train, scoring='precision_weighted', cv=loo)
+	recall = cross_validation.cross_val_score(regr, X_train, y_train, scoring='recall_weighted', cv=loo)
+	print("ACC: " + str(acc) + "; FK: "+str(fl)+"; Precision: "+str(precision)+ "; recall: "+str(recall))
 
 #======================
 def LinearRegr(X_train, X_test, y_train, y_test):
@@ -28,8 +35,7 @@ def LinearRegr(X_train, X_test, y_train, y_test):
 	regr = LinearRegression()
 	y_pred = regr.fit(X_train, y_train).predict(X_test)
 	debug('Regressao Linear (MSE):' + str(metrics.mean_squared_error(y_test, y_pred)))
-	score = model_selection.cross_val_score(regr, X_train, y_train, scoring='accuracy', cv=5)
-	debug('Regressao Linear (CV):' + str(metrics.mean_squared_error(y_test, y_pred)))
+	Validation(regr, X_train, y_train)
 
 #=======================
 def SvrRegr(X_train, X_test, y_train, y_test):
