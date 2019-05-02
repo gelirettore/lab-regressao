f656d8cf860c5d43d1cce9065a5ec7cceb86aa4b
gelirettore <gelirettore@gmail.com>
2019-05-02 16:56:01 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -107,10 +107,10 @@ def main():
 
 	dados = pd.read_csv(data)
 	y1 = dados['f3'].values.reshape(-1,1)
-	y2 = dados['f5'].values.reshape(-1,1)
+	y2 = dados['f4'].values.reshape(-1,1)
 
-	#f4
-	X = dados[['f4','f6']].values
+	#f4, f5,f6,f9,f10,f11,f12
+	X = dados[['f4','f5','f6','f9','f10','f11','f12']].values
 	
 	X_t, X_test, y1_t, y1_test, y2_t, y2_test = train_test_split(X, y1, y2, test_size=0.5, random_state=48)
 	X_train, X_val, y1_train, y1_val, y2_train, y2_val = train_test_split(X_t, y1_t, y2_t, test_size=0.3, random_state=48)
