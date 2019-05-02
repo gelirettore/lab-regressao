c959638d736d6448d6da2275e863ddcc78aa09cc
gelirettore <gelirettore@gmail.com>
2019-05-02 16:45:14 -0300

    :/

--- error.R
+++ error.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 ds <- read_csv("test.csv")
 c<-ggplot(data=ds, aes(error))
-c+geom_density(kernel="gaussian")
-
+c+geom_density(kernel="gaussian", alpha=0.2) + 
+  geom_vline(aes(xintercept=quantile(ds$error)[2]), size=1)+
+  geom_vline(aes(xintercept=quantile(ds$error)[4]), size=1)
