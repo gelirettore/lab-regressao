2d6b3e4ebd12026d1f76df22f33a172a8433d98d
gelirettore <gelirettore@gmail.com>
2019-05-03 09:55:31 -0300

    :/

--- error.R
+++ error.R
@@ -1,7 +1,11 @@
 library(ggplot2)
+library(plyr)
 lr <- read_csv("lr.csv")
-c<-ggplot(data=lr, aes(error))
-c+geom_density(kernel="gaussian", alpha=0.2) + 
-  geom_vline(aes(xintercept=quantile(lr$error)[2]), size=1)+
-  geom_vline(aes(xintercept=quantile(lr$error)[4]), size=1)
+c=ggplot(lr) +
+  geom_histogram(aes(x=error, y=..density..), color="black", fill=NA,binwidth = 0.1)+
+  geom_density(aes(x=error), fill="cyan", alpha=.15)+
+  geom_vline(aes(xintercept=mean(error)), linetype="dashed")+
+  geom_vline(aes(xintercept=mean(error)-abs(sd(lr$error))), linetype="dashed", color="red")+
+  geom_vline(aes(xintercept=mean(error)+abs(sd(lr$error))), linetype="dashed", color="blue")
+c
 #ver sumaraze/sumarize
