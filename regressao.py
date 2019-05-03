e9ba33e712fd645d95d6bb764896257906f73d07
gelirettore <gelirettore@gmail.com>
2019-05-03 14:34:13 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -21,22 +21,13 @@ tracefile = "regressao.csv"
 kneighbors = 3
 jobs = 90
 
-debug("Carregando dados")
-#carrega dados e divide
-
 dados = pd.read_csv(data)
 y1 = dados['f3'].values.reshape(-1,1)
 y2 = dados['f4'].values.reshape(-1,1)
-
-#f4, f5,f6,f9,f10,f11,f12  dsds
 X = dados[['f5','f6']].values
-
 
 X_t, X_test, y1_t, y1_test, y2_t, y2_test = train_test_split(X, y1, y2, test_size=0.5, random_state=48)
 X_train, X_val, y1_train, y1_val, y2_train, y2_val = train_test_split(X_t, y1_t, y2_t, test_size=0.3, random_state=48)
-#criar vetores de teste, mudar teste para validacao
-#nao usar cross validation
-#
 
 #normalizando dados
 min_max_scaler = preprocessing.MinMaxScaler()
