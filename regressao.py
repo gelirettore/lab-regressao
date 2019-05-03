09d6b3837da8edef58c58096a95e04f2b36d3984
gelirettore <gelirettore@gmail.com>
2019-05-03 11:02:53 -0300

    :/

--- regressao.py
+++ regressao.py
@@ -150,8 +150,8 @@ def main():
 	#saveresults("Linear Regression", mse1, var1, mse2, var2)
 	debug("Linear ["+str(mse1)+","+ str(var1)+","+ str(mse2)+","+ str(var2)+"]")
 	#===
-	#regr = knnRegr()
-	#(mse1, var1) = Predict(regr, X_train, X_val, y1_train, y1_val, 'knn1')
+	regr = knnRegr()
+	(mse1, var1) = Predict(regr, X_train, X_val, y1_train, y1_val, 'knn1')
 	#(mse2, var2) = Predict(regr, X_train, X_val, y2_train, y2_val, 'knn2')
 	#saveresults("KNN", mse1, var1, mse2, var2)
 	#debug("KNN ["+str(mse1)+","+ str(var1)+","+ str(mse2)+","+ str(var2)+"]")
